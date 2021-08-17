32779d61f46dd5cddb3faee37e7e3c14563078a0
Douglas Hennrich <douglashennrich@yourdev.com.br>
2021-08-17 17:56:43 -0300

    adicionando NextLive cells e lógica

--- YDSpacey/View/Cell/NextLive/SpaceyNextLiveWithTitleAndButtonCollectionViewCell.swift
+++ YDSpacey/View/Cell/NextLive/SpaceyNextLiveWithTitleAndButtonCollectionViewCell.swift
@@ -115,6 +115,7 @@ class SpaceyNextLiveWithTitleAndButtonCollectionViewCell: UICollectionViewCell {
 // MARK: UI
 extension SpaceyNextLiveWithTitleAndButtonCollectionViewCell {
   private func configureLayout() {
+    configureTitle()
     configureNextLiveView()
     configureButton()
   }
